524a88105be0e8f6c263b7205b31e9e73a221dff
ken <e.kfc47@gmail.com>
2023-10-11 06:51:36 +0900

    dbをsqliteからpostgresqlに

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2023_10_09_061709) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
