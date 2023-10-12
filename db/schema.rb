a87c1282cbdd8bd1dcc5d55b45a35ae0725e0d00
ken <e.kfc47@gmail.com>
2023-10-13 05:25:33 +0900

    profileモデルを追加（routes、コントローラ、ビュー）

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_09_061709) do
+ActiveRecord::Schema.define(version: 2023_10_12_201301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "introduction"
+    t.integer "gender"
+    t.date "birthday"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
