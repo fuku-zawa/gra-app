76fe6f1ab8acf72b914d84deb5f486b6e23bbe15
ken <e.kfc47@gmail.com>
2023-10-28 09:06:20 +0900

    newぺーじの作成、モデルにcontentカラム追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,9 @@
 class CommentsController < ApplicationController
   def new
-    
+    # postを取得（コメントをしたい投稿のidをパラメータから取得）
+    post = Post.find(params[:post_id])
+    # ↑のpostから空のオブジェクトを作成→form_withからcreateアクションに送られる
+    @comment = post.comments.build
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_27_232402) do
+ActiveRecord::Schema.define(version: 2023_10_28_000302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2023_10_27_232402) do
     t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "content", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
