85ecce92ea136afdfb2ee07cd0e33234ef329e97
ken <e.kfc47@gmail.com>
2023-11-12 14:08:38 +0900

    コメントにユーザ名が入っているとその人にメールを送る

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,8 +1,9 @@
 class CommentMailer < ApplicationMailer
-  def new_comment(user, comment)
-    @user = user
+  def new_comment(name, email, comment)
+    @name = name
+    @email = email
     @comment = comment
-    mail to: user.email, subject: "【お知らせ】あなたに対するコメントがありました"
+    mail to: email, subject: "【お知らせ】あなたに対するコメントがありました"
   end
 
 end
