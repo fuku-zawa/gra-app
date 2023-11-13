c1640101271cf01202ce79e657d865c4f6c89534
ken <e.kfc47@gmail.com>
2023-11-14 06:20:23 +0900

    followボタンからフォローできるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,7 +56,12 @@ class User < ApplicationRecord
 
   # フォロー機能
   def follow!(user)
-    following_relationships.create!(following_id: user.id)
+    if user.is_a?(User)
+      user_id = user.id
+    else
+      user_id = user
+    end
+    following_relationships.create!(following_id: user_id)
   end
 
   # フォローを外す機能
